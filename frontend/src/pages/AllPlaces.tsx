a0f0654ab78347e7b60d8fe7210c6c8e13fb3689
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-10-23 23:41:33 +0530

    single palce

--- frontend/src/pages/AllPlaces.tsx
+++ frontend/src/pages/AllPlaces.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { SinglePlace } from "./SinglePlace";
 
 const AllPlaces = () => {
-  const [places, setPlaces] = useState([]);
+  const [places, setPlaces] = useState<SinglePlace[]>([]);
   useEffect(() => {
     (async () => {
       const { data } = await axios.get("http://localhost:8000/api/places", {
@@ -16,17 +17,25 @@ const AllPlaces = () => {
     <div>
       <div className="mt-4">
         {places && places.length > 0 ? (
-          places.map((place) => (
-            <Link key={place._id} to={`/account/places/list/${place._id}`} className="flex cursor-pointer my-2 gap-4 bg-gray-200 p-4 rounded-xl">
-              <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+          places.map((place: SinglePlace) => (
+            <Link
+              key={place._id}
+              to={`/account/places/list/${place._id}`}
+              className="flex cursor-pointer my-2 gap-4 bg-gray-200 p-4 rounded-xl"
+            >
+              <div className="w-32 max-w-lg h-32 bg-gray-300 grow shrink-0">
                 {place.photos.length > 0 && (
-                  <img src={place.photos[0]} alt="places" />
+                  <img
+                    src={place.photos[0]}
+                    alt="places"
+                    className="w-full h-full object-cover"
+                  />
                 )}
               </div>
-             <div className="grow-0 shrink">
-             <h2 className="text-xl font-semibold"> {place?.title}</h2>
-             <p className="text-sm mt-e">{place.description}</p>
-             </div>
+              <div className="grow-0 shrink">
+                <h2 className="text-xl font-semibold"> {place?.title}</h2>
+                <p className="text-sm mt-e">{place.description}</p>
+              </div>
             </Link>
           ))
         ) : (
--- frontend/src/pages/SinglePlace.tsx
+++ frontend/src/pages/SinglePlace.tsx
@@ -1,10 +1,41 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { apiServer } from "../config/config";
+export interface SinglePlace {
+  address: string;
+  checkIn: string;
+  checkOut: string;
+  createdAt: string;
+  description: string;
+  extraInfo: string;
+  maxGuest: number;
+  owner: string;
+  perks: [string];
+  photos: [string];
+  title: string;
+  updatedAt: string;
+  __v: number;
+  _id: string;
+}
 
 const SinglePlace = () => {
-  const [place, setPlace] = useState({});
+  const [place, setPlace] = useState<SinglePlace>({
+    address: "",
+    checkIn: "",
+    checkOut: "",
+    createdAt: "",
+    description: "",
+    extraInfo: "",
+    maxGuest: 0,
+    owner: "",
+    perks: [""],
+    photos: [""],
+    title: "",
+    updatedAt: "",
+    __v: 0,
+    _id: "",
+  });
   const { id } = useParams();
 
   useEffect(() => {

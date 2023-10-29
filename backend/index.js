481900e11b8d2a9d1d2fb0193dffb62d3afa5caf
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-10-30 00:09:15 +0530

    front end url updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-mern-frontend.vercel.app/",
   })
 );
 app.use(cookieParser());
